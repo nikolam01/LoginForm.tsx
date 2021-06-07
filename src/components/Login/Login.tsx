3a91be8cc66f34082cf261b02f16ca4a8e6b6f49
Nikola Mirilo <nikolamirilo@gmail.com>
2021-06-07 15:06:44 +0200

    changed css

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,11 +3,13 @@ import "./Login.css";
 
 const Login = () => {
   return (
-    <div className="login">
-      <h1>Login</h1>
-      <input className="email" placeholder="Enter email"></input>
-      <input className="password" placeholder="Enter password"></input>
-      <button className="submit">Submit</button>
+    <div id="loginSection">
+      <form className="loginForm">
+        <h1>Login</h1>
+        <input className="email" placeholder="Enter email"></input>
+        <input className="password" placeholder="Enter password"></input>
+        <button className="submit">Submit</button>
+      </form>
     </div>
   );
 };
